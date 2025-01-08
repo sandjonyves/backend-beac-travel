fa2c6c83cd0626f1f2bafe8aea95992d1f8915fe
sandjonyves <117326116+sandjonyves@users.noreply.github.com>
2025-01-08 17:08:13 +0100

    Update settings.py for database

--- BeacBackend/settings.py
+++ BeacBackend/settings.py
@@ -101,27 +101,27 @@ WSGI_APPLICATION = 'BeacBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#   'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': env('PGDATABASE'),
-#     'USER': env('PGUSER'),
-#     'PASSWORD': env('PGPASSWORD'),
-#     'HOST': env('PGHOST'),
-#     'PORT': env('PGPORT'),
-#     'OPTIONS': {
-#       'sslmode': 'require',
-#     },
-#   }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': env('PGDATABASE'),
+    'USER': env('PGUSER'),
+    'PASSWORD': env('PGPASSWORD'),
+    'HOST': env('PGHOST'),
+    'PORT': env('PGPORT'),
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+  }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
